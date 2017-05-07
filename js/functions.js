ed00a5a990e7811e9f9a60c8175e43d60d8ed1ab
DESKTOP-EJHEJCC\Antquin <giorgi.abjandadze05@geolab.edu.ge>
2017-05-07 14:11:08 +0400

    ცვლილებები შევიდა ჯავასკრიპტში, რესპონსივსა და მარქაფში.

--- js/functions.js
+++ js/functions.js
@@ -4,10 +4,12 @@ $(document).ready(function () {
         if ($(this).hasClass('btnact')) {
             $(this).removeClass('btnact');
             $('header > section.HeaderContainer > ul.respMenu').removeClass('active');
+            $('body').css({ position: 'initial' });
         }
         else {
             $(this).addClass('btnact');
             $('header > section.HeaderContainer > ul.respMenu').addClass('active');
+        $('body').css({ position: 'fixed' });
         }
     });
 });
